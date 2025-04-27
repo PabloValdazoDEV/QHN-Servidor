0c2032b2fccc0346534448b215eac1b7d897fc70
PabloValdazoDEV <157512231+PabloValdazoDEV@users.noreply.github.com>
2025-04-27 22:13:23 +0200

    feat: protección del login

--- router/auth.js
+++ router/auth.js
@@ -3,18 +3,26 @@ const router = express.Router();
 const prisma = require("../prisma/prisma");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { DateTime } = require("luxon");
 const authMiddleware = require("../middelwares");
 
 router.post("/register", authMiddleware, async (req, res) => {
   const { email, password, name, role } = req.body;
-  const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/;
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordClean = password.trim();
+  const emailClean = email.trim();
 
   try {
-    if (!regex.test(passwordClean) || !email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Invalid email or password format" });
+    if (
+      !passwordRegex.test(passwordClean) ||
+      !emailRegex.test(emailClean) ||
+      !password ||
+      !email
+    ) {
+      return res.status(400).json({
+        message: "El formato de la contraseñao del email no es valida",
+      });
     }
 
     const hashedPassword = await bcrypt.hash(passwordClean, 10);
@@ -43,8 +51,53 @@ router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await prisma.user.findUnique({ where: { email } });
+
+    if (user === null) {
+      return res
+        .status(401)
+        .json({ message: "Ese correo no esta registrado." });
+    }
+
+    const ahora = DateTime.now().setZone("Europe/Madrid");
+
+    const haceDiezMinutos = ahora.minus({ minutes: 10 }).toJSDate();
+
+    const erroresLogin = await prisma.errorLogin.findMany({
+      where: {
+        user_id: user.id,
+        date_try: {
+          gte: haceDiezMinutos,
+        },
+      },
+      orderBy: { date_try: "desc" },
+    });
+
+    if (erroresLogin.length >= 3) {
+      return res.status(401).json({
+        message:
+          "Ha superado el número máximo de intentos. Intentelo más tarde.",
+      });
+    }
+
     if (!user || !(await bcrypt.compare(password.trim(), user.password))) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      await prisma.errorLogin.create({
+        data: {
+          user_id: user.id,
+          date_try: DateTime.now().setZone("Europe/Madrid").toJSDate(),
+        },
+      });
+      if (erroresLogin.length >= 2) {
+        return res.status(401).json({
+          message:
+            "Ha superado el número máximo de intentos. Intentelo más tarde.",
+        });
+      }
+      return res.status(401).json({
+        message:
+          "Credenciales invalidas, tienes " +
+          (erroresLogin.length == "null" ? 2 : 2 - erroresLogin.length) +
+          " intentos.",
+      });
     }
 
     const token = jwt.sign(
@@ -52,14 +105,13 @@ router.post("/login", async (req, res) => {
         id: user.id,
         email: user.email,
         name: user.name,
-        role: user.role
+        role: user.role,
       },
       process.env.JWT_SECRET,
       {
-        expiresIn: "1h"
+        expiresIn: "1h",
       }
     );
-    
 
     res.json({ message: "Login successful", token });
   } catch (error) {
@@ -75,15 +127,15 @@ router.get("/me", authMiddleware, async (req, res) => {
   try {
     const user = await prisma.user.findFirst({
       where: {
-        id: req.user.id
+        id: req.user.id,
       },
-      select:{
+      select: {
         email: true,
         role: true,
-        name: true, 
-        id: true
-      }
-    })
+        name: true,
+        id: true,
+      },
+    });
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -94,8 +146,6 @@ router.get("/me", authMiddleware, async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Error retrieving user" });
   }
-  
 });
 
 module.exports = router;
-
